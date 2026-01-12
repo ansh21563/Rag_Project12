7c6d97a1a6ca1332e16e8281f9d4debb19a5976e
Ansh Aggarwal <anshagg410@gmail.com>
2026-01-12 13:57:45 +0530

    adding jsons file

--- read_chunks.py
+++ read_chunks.py
@@ -1,13 +1,35 @@
 import requests
+import os
+import json
 
-def create_embedding(text):
-    r = requests.post("http://localhost:11434/api/embeddings",json={
+def create_embedding(text_list):
+    r = requests.post("http://localhost:11434/api/embed",json={
         "model": "bge-m3",
-        "prompt":text
+        "input":text_list
     })
 
-    embedding = r.json()['embedding']
+    embedding = r.json()['embeddings']
     return embedding
 
-a = create_embedding("cat sat on the mat")
-print(a)
+
+
+
+jsons = os.listdir("jsons") # List all the jsons 
+my_dicts = []
+chunk_id = 0
+
+for json_file in jsons:
+    with open (f"jsons/{json_file}") as f:
+        content = json.load(f)
+    embediings = create_embedding([c['text'] for c in content ['chunks']])    
+    for i, chunk in enumerate(content['chunks']):
+        chunk['chunk_id'] = chunk_id
+        chunk['embeddings'] = (chunk['text'])
+        chunk_id += 1
+        my_dicts.append(chunk)
+        print(chunk)   
+    break
+
+print(my_dicts)
+# a = create_embedding("cat sat on the mat")
+# print(a)
