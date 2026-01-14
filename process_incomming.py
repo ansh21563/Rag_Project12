cb5f4989d1121d10e89a7cf5acf9105e6b8597c1
Ansh Aggarwal <anshagg410@gmail.com>
2026-01-14 15:24:36 +0530

    adding the embedding file

---
+++ process_incomming.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+import joblib
+from sklearn.metrics.pairwise import cosine_similarity
+import requests
+# from read_chunks import create_embedding
+
+
+def create_embedding(text_list):
+    r = requests.post("http://localhost:11434/api/embed",json={
+        "model": "bge-m3",
+        "input":text_list
+    })      
+
+    embedding = r.json()['embeddings']
+    return embedding
+
+df = joblib.load('embeddings.joblib')
+
+incoming_query =  input(" Ask a question: ")
+question_embedding = create_embedding([incoming_query])[0] 
+# print(question_embedding)
+# print(my_dicts)
+# a = create_embedding("cat sat on the mat")
+# print(a)
+
+
+# find similarities of question_embedding with other embeddings
+# print(np.vstack(df['embedding'].values))
+# print(np.vstack(df['embedding'].shape))
+similarities = cosine_similarity(np.vstack(df['embedding']),[question_embedding]).flatten()
+# print(similarities)
+top_results = 3
+max_indx = similarities.argsort()[::-1][0:top_results]
+print(max_indx)
+new_df = df.loc[max_indx]
+# print(similarities.argsort()) 
+# print(new_df[["title","number","text"]])
+
+
+for index, item in new_df.iterrows():
+    print(index,item["title"],item["number"],item["text"])            
--- read_chunks.py
+++ read_chunks.py
@@ -1,6 +1,11 @@
 import requests
 import os
 import json
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+import joblib
+
 
 def create_embedding(text_list):
     r = requests.post("http://localhost:11434/api/embed",json={
@@ -11,9 +16,6 @@ def create_embedding(text_list):
     embedding = r.json()['embeddings']
     return embedding
 
-
-
-
 jsons = os.listdir("jsons") # List all the jsons 
 my_dicts = []
 chunk_id = 0
@@ -21,15 +23,16 @@ chunk_id = 0
 for json_file in jsons:
     with open (f"jsons/{json_file}") as f:
         content = json.load(f)
-    embediings = create_embedding([c['text'] for c in content ['chunks']])    
+    print(f"Creating Embeddings for {json_file}")    
+    embeddings = create_embedding([c['text'] for c in content ['chunks']])
+         
     for i, chunk in enumerate(content['chunks']):
         chunk['chunk_id'] = chunk_id
-        chunk['embeddings'] = (chunk['text'])
+        chunk['embedding'] = embeddings[i]
         chunk_id += 1
         my_dicts.append(chunk)
-        print(chunk)   
-    break
-
-print(my_dicts)
-# a = create_embedding("cat sat on the mat")
-# print(a)
+        # print(chunk)   
+   
+df=pd.DataFrame.from_records(my_dicts)
+# print(df)
+joblib.dump(df,'embeddings.joblib') 
